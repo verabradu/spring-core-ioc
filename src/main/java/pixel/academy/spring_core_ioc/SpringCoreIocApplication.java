88597984d1af9cf138ef6e6dd264ae24b6678e00
verabradu <verabradu92@gmail.com>
2025-05-26 15:19:53 -0500

    Add Sring Boot project

---
+++ src/main/java/pixel/academy/spring_core_ioc/SpringCoreIocApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.spring_core_ioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreIocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreIocApplication.class, args);
+	}
+
+}
