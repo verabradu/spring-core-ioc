7293dfac6c66553b96333b467b1103d1b272fad3
verabradu <verabradu92@gmail.com>
2025-05-26 17:01:02 -0500

    Initial Spring IoC project

--- src/main/java/pixel/academy/spring_core_ioc/SpringCoreIocApplication.java
+++ src/main/java/pixel/academy/spring_core_ioc/SpringCoreIocApplication.java
@@ -3,7 +3,9 @@ package pixel.academy.spring_core_ioc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"pixel.academy.spring_core_ioc," + "util"})
+
+
 public class SpringCoreIocApplication {
 
 	public static void main(String[] args) {
